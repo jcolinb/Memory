befcd5fc716fe172731e71056b512c6e843da0db
c mulligan <gateless@protonmail.com>
2018-06-17 22:11:10 -0400

    add Clone(); finish Propogate()

--- js/script.js
+++ js/script.js
@@ -1,16 +1,42 @@
 const Board = document.getElementById('container');
 const Start = document.getElementById('start');
-const Deck  = document.getElementsByClassName('.card');
 
 const Areas = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'];
 
-Start.addEventListener('click',propogate);
+const Clone = function (arr) {
+  let a = arr;
+  let b = [];
 
-propogate = function () {
-  let mixedAreas = Shuffler.shuff(Areas);
-  for (i in Deck) {
-    i.setAttribute('grid-area',mixedAreas.shift());
+  for (i in a) {
+    b[i] = a[i];
   }
+  return b;
 };
 
-const Shuffler = require('./shuff.js');
+const Shuff = function (arr) {
+  let a = Clone(arr);
+  let b = [];
+  for (i=a.length;i>0;i--) {
+    r = Math.floor(Math.random() * a.length);    
+    b.push(a[r]);
+    a.splice(r,1);
+  };
+  return b
+};
+
+propogate = function () {
+  console.log("propogating...");
+  Board.innerHTML = "";
+  let Hand = document.createDocumentFragment();
+  let mixedAreas = Shuff(Areas);
+  console.log(mixedAreas);
+    for (a in mixedAreas) {
+    card = document.createElement('div');
+    card.classList.add('card');
+    card.setAttribute('grid-area',mixedAreas[a]);
+    Hand.appendChild(card);
+  }
+  Board.appendChild(Hand);
+};
+
+Start.addEventListener('click',propogate);
