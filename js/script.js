55afd0a32935848435bd4e75d0d2f9fad80bb0ee
c mulligan <gateless@protonmail.com>
2018-06-18 19:26:04 -0400

    add Flip(), test image for card

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const Board = document.getElementById('container');
 const Start = document.getElementById('start');
-
+const Icons = ['A','A','B','B','C','C','D','D','E','E','F','F','H','H','I','I'];
 const Areas = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'];
 
 const Clone = function (arr) {
@@ -24,19 +24,28 @@ const Shuff = function (arr) {
   return b
 };
 
+const Flip = function (e) {
+  if (e.target.id !== 'container')
+  {e.target.classList.toggle('hidden');}
+};
+
 propogate = function () {
   console.log("propogating...");
   Board.innerHTML = "";
   let Hand = document.createDocumentFragment();
   let mixedAreas = Shuff(Areas);
+  let I = Clone(Icons);
+
   console.log(mixedAreas);
     for (a in mixedAreas) {
     card = document.createElement('div');
     card.classList.add('card');
-    card.setAttribute('grid-area',mixedAreas[a]);
+    card.setAttribute('style',`grid-area: ${mixedAreas[a]}`);
+    card.textContent = I[a];
     Hand.appendChild(card);
   }
   Board.appendChild(Hand);
 };
 
 Start.addEventListener('click',propogate);
+Board.addEventListener('click',Flip);
