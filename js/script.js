a7324a90e3e4e9c20fe10aa80ee4f46a874e86c5
c mulligan <gateless@protonmail.com>
2018-06-21 16:43:42 -0400

    fix bugs with Promise flow (game freeze)

--- js/script.js
+++ js/script.js
@@ -6,15 +6,20 @@ const Deck  = {};
 var   Cards = undefined;
 const GM    = {
   stack: [],
+
   are_equal: function (a,b) {return (a == b)},
+
   reset: function () {
     for (a in Deck) {
         if (Deck[a].resolved !== 'yes') {
           Cards[Deck[a].index].textContent = "";
           Cards[Deck[a].index].classList.remove('revealed');
-          }
+          }      
     }
+    GM.stack=[];
+    new Promise((res,rej) => Board.addEventListener('click',res)).then(GM.flip);
   },
+
   deal: function () {
   console.log("propogating...");
   Board.innerHTML = "";
@@ -28,19 +33,45 @@ const GM    = {
       card = document.createElement('div');
       card.classList.add('card');
       card.setAttribute('style',`grid-area: ${areaAssign[a]}`);
-      card.textContent = mixedIcons[a];
       Hand.appendChild(card);
       Deck[areaAssign[a]] = {val:mixedIcons[a], resolved: "no", index:a};
     }
   Board.appendChild(Hand);
   console.log(Deck);
   Cards = document.getElementsByClassName('card');
-},
+  },
+
   flip: function (e) {
-  if ((e.target.id !== 'container') && !(e.target.classList.contains('revealed')))
-  {e.target.classList.add('revealed');}
-  new Promise((res,rej) => Board.addEventListener('click',res)).then(GM.flip);
-},
+  if (e.target.classList.contains( 'card')) {
+    e.target.classList.add('revealed');
+    e.target.textContent = Deck[e.target.style.gridColumnEnd].val;
+    GM.stack.push(e.target);
+    console.log(GM.stack);
+    new Promise((res,rej) => Board.addEventListener('click',res)).then(GM.judge);
+  }
+  else{
+    new Promise((res,rej) => Board.addEventListener('click',res)).then(GM.flip);
+  }
+  },
+
+  judge: function (e) {
+    if ((e.target.classList.contains( 'card')) && (e.target !== GM.stack[0])) {
+      e.target.classList.add('revealed');
+      e.target.textContent = Deck[e.target.style.gridColumnEnd].val;
+
+    let a = e.target;
+    let b = GM.stack[0];
+    if (GM.are_equal(a.textContent,b.textContent)) {
+      Deck[a.style.gridColumnEnd].resolved = 'yes';
+      Deck[b.style.gridColumnEnd].resolved = 'yes';
+    }
+    setTimeout(GM.reset,1000);
+    }      
+    else {
+    new Promise((res,rej) => Board.addEventListener('click',res)).then(GM.judge);
+    }
+  },
+
   shuffle: function (arr) {
   let a = Clone(arr);
   let b = [];
@@ -62,12 +93,6 @@ const Clone = function (arr) {
   }
   return b;
 };
-
-
-
-
-
-
 
 new Promise((res,rej) => Start.addEventListener('click',res)).then(GM.deal);
 new Promise((res,rej) => Board.addEventListener('click',res)).then(GM.flip);
